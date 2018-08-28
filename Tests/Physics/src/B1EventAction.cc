f8e7e8fdb4e73556644b55fba00c2d4ec8fe2e12
FabianKlimpel <fabian.klimpel@tum.de>
2018-08-28 16:27:19 +0200

    HadInt2 copied

---
+++ Tests/Physics/src/B1EventAction.cc
@@ -0,0 +1,70 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id: B1EventAction.cc 93886 2015-11-03 08:28:26Z gcosmo $
+//
+/// \file B1EventAction.cc
+/// \brief Implementation of the B1EventAction class
+
+#include "../include/B1EventAction.hh"
+#include "../include/B1RunAction.hh"
+
+#include <fstream>
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+B1EventAction::B1EventAction(B1RunAction* runAction)
+: G4UserEventAction(),
+  fRunAction(runAction)
+{} 
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+B1EventAction::~B1EventAction()
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void B1EventAction::BeginOfEventAction(const G4Event*)
+{
+  particles.clear();
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void B1EventAction::EndOfEventAction(const G4Event*)
+{
+	std::ofstream ofs("geant4out.txt", std::ofstream::app);
+	for(auto& p : particles)
+	{
+		ofs << p.pdg << "\t" << p.mass << "\t" << p.charge << "\t" << p.energy << "\t"
+			<< p.position[0] << "\t" << p.position[1] << "\t" << p.position[2] << "\t"
+			<< p.momentum[0] << "\t" << p.momentum[1] << "\t" << p.momentum[2] << std::endl;
+	}
+	ofs << "*" << std::endl;
+	ofs.close();
+}
+	
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
