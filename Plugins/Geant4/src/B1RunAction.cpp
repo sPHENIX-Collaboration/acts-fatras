b3be82ac6bae4650e465cea71043a92dcf693e5f
FabianKlimpel <fabian.klimpel@tum.de>
2018-09-11 09:14:23 +0200

    Plugin for G4 added, G4 Detector and PartileGun construction implemented

---
+++ Plugins/Geant4/src/B1RunAction.cpp
@@ -0,0 +1,69 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id: B1RunAction.cc 99560 2016-09-27 07:03:29Z gcosmo $
+//
+/// \file B1RunAction.cc
+/// \brief Implementation of the B1RunAction class
+
+#include "Fatras/Plugins/Geant4/B1RunAction.hpp"
+
+#include "G4RunManager.hh"
+#include "G4Run.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+B1RunAction::B1RunAction()
+: G4UserRunAction()
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+B1RunAction::~B1RunAction()
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void B1RunAction::BeginOfRunAction(const G4Run*)
+{ 
+  // inform the runManager to save random number seed
+  G4RunManager::GetRunManager()->SetRandomNumberStore(false);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void B1RunAction::EndOfRunAction(const G4Run* run)
+{
+  if (run->GetNumberOfEvent() == 0) return;
+        
+  // Print
+  //  
+    G4cout
+     << G4endl
+     << "--------------------End of Global Run-----------------------";
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
